4198fbbd7a385600f699da8a01cee156f87301a8
MarcusV <Marcus-v@outlook.com>
2023-02-13 22:52:20 -0300

    create template

--- core/views.py
+++ core/views.py
@@ -1,11 +1,13 @@
-from typing import Any
+from typing import Any, Dict, List
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request: Any) -> HttpResponse:
-    return render(request, 'index.html')
+    to_template: Dict[str] = {"welcome": 'welcome!'}
+    return render(request, 'index.html', to_template)
 
 
 def another_route(request: Any) -> HttpResponse:
-    return render(request, 'another.html')
+    items: Dict[str, List] = {"items": [1, 2, 3]}
+    return render(request, 'another.html', items)
