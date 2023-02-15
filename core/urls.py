ac5c5281d58d11c46d85b4389738381a3e2e271e
MarcusV <Marcus-v@outlook.com>
2023-02-15 02:27:26 -0300

    data in templates and named parameters

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import index, another_route
+from .views import product
 
 
 urlpatterns = [
-    path('', index),
-    path('another', another_route)
+    path('', index, name='index'),
+    path('another', another_route, name='another'),
+    path('product/<int:id>', product, name='product')
 ]
--- core/views.py
+++ core/views.py
@@ -1,13 +1,26 @@
 from typing import Any, Dict, List
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 
 def index(request: Any) -> HttpResponse:
-    to_template: Dict[str] = {"welcome": 'welcome!'}
+    products: Product = Product.objects.all()
+    to_template: Dict[str] = {
+        "welcome": f'welcome {request.user}!',
+        "products": products
+    }
     return render(request, 'index.html', to_template)
 
 
 def another_route(request: Any) -> HttpResponse:
     items: Dict[str, List] = {"items": [1, 2, 3]}
     return render(request, 'another.html', items)
+
+
+def product(request: Any, id: int) -> HttpResponse:
+    print(f"ID {id}")
+    prod = Product.objects.get(id=id)
+    prodct: Dict[str, Any] = {"product": prod}
+    return render(request, 'product.html', prodct)
+
